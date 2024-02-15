d8c93b496556ad0f85c460f97572b99db0dc11b2
simmi <22905246+noepse@users.noreply.github.com>
2024-02-15 14:50:34 +0000

    add routing, styling, tests

--- App.jsx
+++ App.jsx
@@ -1,16 +1,93 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
-import ClickButton from './Button'
+// import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button, TextInput, Pressable} from 'react-native';
+import * as React from "react";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import CustomButton from './CustomButton';
+import { useState, useCallback } from 'react';
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <Text style={styles.text}>heloo</Text>
-      <ClickButton />
-    </View>
+    //           <View style={styles.container}>
+    //   <Text>Hello</Text>
+    //   <Text style={styles.text}>heloo</Text>
+    //   <ClickButton />
+    //   <Button
+    //   title="Go to Home"
+    //   onPress={() =>
+    //     navigation.navigate('Home', {name: 'Jane'})
+    //   }
+    // />
+    // </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="About" component={About} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <>
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', {name: 'Jane'})
+      }
+    />
+    <Button
+    title="Go to About page"
+    onPress={() =>
+      navigation.navigate('About', {page: 'About'})
+    }
+  />
+  <CustomButton />
+    </>
+  );
+};
+const ProfileScreen = ({navigation, route}) => {
+      const [groceryItem, setGroceryItem] = useState('');
+      const [items, setItems] = useState([]);
+    
+      const addNewItemToShoppingList = useCallback(() => {
+        setItems([groceryItem, ...items]);
+        setGroceryItem('');
+      }, [groceryItem, items]);
+    
+      return (
+
+        <>
+          <TextInput
+            value={groceryItem}
+            placeholder="Enter grocery item"
+            onChangeText={text => setGroceryItem(text)}
+          />
+          <Button 
+            style = {styles.button}
+            title="Add the item to list"
+            onPress={addNewItemToShoppingList}
+          />
+          {items.map((item, index) => (
+            <Text key={index}>{item}</Text>
+          ))}
+
+        </>
+      );
+    }
+
+const About = ({navigation, route}) => {
+  return <Text>This is {route.params.page} page</Text>;
+};
 
 // React Native Styles
 const styles = StyleSheet.create({
@@ -21,5 +98,11 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#F2590C',
+  },
+  button: {
+    color: 'red',
+    backgroundColor: 'pink',
+    width: '50%',
+    padding: '1em'
   }
 });
